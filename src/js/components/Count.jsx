06c986ad4dc328b91e9672f2b0f4eca63eecf151
Cere <c_cere2909@hotmail.com>
2025-04-05 11:20:33 +0000

    terminado

--- src/js/components/Count.jsx
+++ src/js/components/Count.jsx
@@ -2,10 +2,12 @@ import React from "react";
 
 export const Count = (props) => {
     return (
-        <div className="bg-dark text-white">
+        <div className="marco">
+        <div className="clock bg-dark text-white">
             <p className="fs-1">
                 {props.countValue}
             </p>
+        </div>
         </div>
     );
 };
